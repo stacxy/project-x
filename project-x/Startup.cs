991f6d703cde980adf4f1d11b2fd5badaf1adc2b
stacey <staceystroh.84@gmail.com>
2021-03-25 23:02:44 -0500

    reconfig for MySql - remove migration. add pomelo. change default connect.

--- project-x/Startup.cs
+++ project-x/Startup.cs
@@ -27,11 +27,11 @@ namespace project_x
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseMySql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<AppDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
